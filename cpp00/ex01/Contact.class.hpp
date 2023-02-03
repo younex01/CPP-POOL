064c8ae88288149e82b62a906ad91211cd6b4c80
Youness Elousse <yelousse@e1r7p3.1337.ma>
2023-02-03 01:49:44 +0100

    second push

--- cpp00/ex01/Contact.class.hpp
+++ cpp00/ex01/Contact.class.hpp
@@ -1,10 +1,13 @@
 #ifndef CONTACT_CLASS_HPP
 #define CONTACT_CLASS_HPP
+#include <iostream>
 
 class Contact
 {
 private:
-	
+	std::string _fs_name;
+	std::string	_ls_name;
+	int			_age;
 public:
 	Contact();
 	~Contact();
